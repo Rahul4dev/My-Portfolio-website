444c5f67a07ed416c538c1eb151488ae24e8e6f9
Rahul4dev <rahulsinghsignin@gmail.com>
2023-03-29 00:08:32 +0530

    backend code of portfolio site

--- schemas/index.js
+++ schemas/index.js
@@ -1,1 +1,19 @@
-export const schemaTypes = []
+import works from './works'
+import testimonials from './testimonials'
+import brands from './brands'
+import about from './about'
+import experiences from './experiences'
+import skills from './skills'
+import workExperience from './workExperience'
+import contact from './contact'
+
+export const schemaTypes = [
+  works,
+  testimonials,
+  brands,
+  about,
+  skills,
+  workExperience,
+  experiences,
+  contact,
+]
